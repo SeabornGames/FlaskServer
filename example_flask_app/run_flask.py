90a12322075889f755024a72ffa5e01e06e20136
MichaelChristenson <mochristenson@gmail.com>
2018-01-04 16:58:16 -0600

    Import issues again

--- example_flask_app/run_flask.py
+++ example_flask_app/run_flask.py
@@ -2,30 +2,26 @@
 import os
 import sys
 import traceback
-from seaborn.logger import log
+#1from seaborn.logger import log
 
 # This is needed so endpoints can all import the same global_import
 root_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(root_path))
-#log = SeabornLogger(__name__)
 
 stage = "importing global_import"
 try:
     from settings.global_import import setup_flask
     stage = "importing endpoints"
-    #if __name__ == '__main__':
     import endpoints
-    #else:
-    #from endpoints import endpoints
     stage = "setup flask"
     run = setup_flask.setup_run(endpoints)
 except Exception as ex:
     msg = "Exception in %s with %s\n\n%s" % (
         stage, ex, traceback.format_exc())
-    log.critical(msg)
+    #1log.critical(msg)
     print(msg)
     sys.exit()
 
 if __name__ == '__main__':
-    log.debug("Starting Flask Service from Run")
+    #1log.debug("Starting Flask Service from Run")
     run()
--- example_flask_app/settings/global_import.py
+++ example_flask_app/settings/global_import.py
@@ -2,14 +2,15 @@
     This module will be imported by each of the endpoint views and models,
     so it will contain anything they will generally need
 """
+
+#1from seaborn.logger import log
 from .config import configuration
 from seaborn.flask_server.setup.setup_flask import SetupFlask
 from seaborn.flask_server.blueprint.blueprint import ProxyEndpoint
 
-log = configuration.setup_logging()
 setup_flask = SetupFlask(configuration)
 app = setup_flask.app
 db = setup_flask.db
 conn = ProxyEndpoint()
 
-from seaborn.flask_server import *
+from seaborn.flask_server.global_import import *
